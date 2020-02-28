913797faf59dd57ea5ceeef366e29edb3e87a493
Rafael Bernabeu <rbbernabeu@gmail.com>
2020-02-28 02:01:48 -0300

    reorg

--- open-cv.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-cap = cv.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv.destroyAllWindows()
